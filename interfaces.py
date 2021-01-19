81a7a4450a2df7b0e4ff74389bd7a05767744d9e
Alexander Stevenson -T (alexstev - COLLABERA INC at Cisco) <alexstev@cisco.com>
2021-01-19 16:04:36 -0500

    Have the running-config being appended to a text file with timestamp

--- interfaces.py
+++ interfaces.py
@@ -1,14 +1,31 @@
 
 from netmiko import ConnectHandler
+from prettytable import PrettyTable
 
-
+# function for displaying interfaces
 def interfaces(username, password):
+    # username and password defined in main.py
     device = ConnectHandler(device_type='cisco_ios', host='ios-xe-mgmt.cisco.com', port=8181, username=username, password=password)
     output = device.send_command("show interfaces")
     interface_list = []
     output_list = output.split()
+    # create a ist of interfaces
     for word in output_list:
         if 'GigabitEthernet' in word:
-            interface_list.append(word)      
-    print(interface_list)
+            interface_list.append(word) 
+    # print list of interfaces
+    print("\n\n\n")
+    print("INTERFACES")
+    print("----------") 
+    print()    
+    item_num = 1 
+    for a_device in interface_list:
+        print('________________________')
+        print(item_num, "  " + a_device)
+        print('************************')
+        print()
+        item_num += 1
+        # prompt user to select interface
+    choice = int(input("Please select an interface: "))
+
     device.disconnect()
--- main.py
+++ main.py
@@ -5,21 +5,48 @@ import re
 from Gelatin.util import compile, generate
 from prettytable import PrettyTable
 from interfaces import interfaces
-
+import os
+import datetime
 # prompt for authorization credentials
 username = input("Username: ")
 password = getpass("Password: ")
 
 #menu
 menu = PrettyTable(['OPTION', 'CONFIG'])
-menu.add_row(["1", "NTP"])
+menu.add_row(["1", "Running Config"])
 menu.add_row(["2", "Interfaces"])
-menu.add_row(["3", "LOGGING"])
+menu.add_row(["3", ""])
 print(menu)
 choice = int(input("Please select an option: "))
-
+os.system('clear')
 
 # device = ConnectHandler(device_type='cisco_ios', host='ios-xe-mgmt.cisco.com', port=8181, username=username, password=password)
+
+# Function to run if user selects #1 from main menu
+def running_config(usename, password):
+    run_config_device = ConnectHandler(device_type='cisco_ios', host='ios-xe-mgmt.cisco.com', port=8181, username=username, password=password)
+    run_config_output = run_config_device.send_command("show running-config")
+    print(run_config_output)
+    print()
+    print('***************************************************************************')
+    print('A COPY OF THIS RUNNING CONFIGURATION HAS BEEN LOGGED TO: runnin_config.txt')
+    print('--------------------------------------------------------')
+    print('***************************************************************************')
+    print()
+    runnin_config_file = open("runnin_config.txt", "a")
+    now = datetime.datetime.now()
+    runnin_config_file.write('\n' + '*********************************************************\n')
+    timestamp = str(now.strftime("%Y%m%d_%H:%M:%S"))
+    runnin_config_file.write(timestamp + '\n')
+    runnin_config_file.write('*********************************************************\n')
+    runnin_config_file.write(run_config_output)
+    runnin_config_file.close()
+    run_config_device.disconnect()
+    
+
+
+if choice == 1:
+    running_config(username, password)
 
 if choice == 2:
     interfaces(username, password)
@@ -31,8 +58,6 @@ if choice == 2:
 
 
 
-# text_file = open("output.txt", "w")
-# text_file.write(output)
-# text_file.close()
 
-device.disconnect()
+
+
